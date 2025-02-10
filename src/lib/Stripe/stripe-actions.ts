4f3b435c198f67a02e73a631cf8775e8820b612c
ABSOLUTE-NOTHING <116956963+ABSOLUTE-NOTHING@users.noreply.github.com>
2025-02-10 16:12:15 +0530

    Build fixes 3

--- src/lib/Stripe/stripe-actions.ts
+++ src/lib/Stripe/stripe-actions.ts
@@ -1,8 +1,7 @@
 "use server";
 import Stripe from "stripe";
 import { db } from "../db";
-import { stripe } from "@/lib/stripe";
-
+import { stripe } from "@/lib/Stripe";
 export const subscriptionCreated = async (
   subscription: Stripe.Subscription,
   customerId: string
