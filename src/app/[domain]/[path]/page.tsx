97dbb8247faa63cc535508bc90c892c4f572a38a
ABSOLUTE-NOTHING <116956963+ABSOLUTE-NOTHING@users.noreply.github.com>
2025-02-12 20:24:17 +0530

    added subdomain hosting feature (path routes enabled for hosting)

--- src/app/[domain]/[path]/page.tsx
+++ src/app/[domain]/[path]/page.tsx
@@ -1,9 +1,31 @@
+import FunnelEditor from "@/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/_components/funnel-editor";
+import { getDomainContent } from "@/lib/queries";
+import EditorProvider from "@/providers/editor/editor-provider";
+import { notFound } from "next/navigation";
 import React from "react";
 
-type Props = {};
+type Props = {
+  domain: string;
+  path: string;
+};
 
-const page = (props: Props) => {
-  return <div>page</div>;
+const page = async ({ params }: { params: Props }) => {
+  const domainData = await getDomainContent(params.domain.slice(0, -1));
+  const pageData = domainData?.FunnelPages.find(
+    (page) => page.pathName === params.path
+  );
+
+  if (!pageData || !domainData) return notFound();
+
+  return (
+    <EditorProvider
+      subaccountId={domainData.subAccountId}
+      pageDetails={pageData}
+      funnelId={domainData.id}
+    >
+      {pageData && <FunnelEditor pageId={pageData.id} live={true} />}
+    </EditorProvider>
+  );
 };
 
 export default page;
